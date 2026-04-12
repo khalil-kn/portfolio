28f5171afc65abaaf46a3166512f757ca4dd3a25
Med Khalil Knaissi <khalil.knaissi9@gmail.com>
2026-04-12 14:04:47 +0100

    Adding all projects page

--- config/core/urls.py
+++ config/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('project/<int:id>/', views.project_detail, name='project_detail'),
+    path('projects/', views.projects_list, name='projects_list'),
 ]
--- config/core/views.py
+++ config/core/views.py
@@ -3,7 +3,7 @@ from .models import Project
 
 
 def home(request):
-    projects = Project.objects.all().order_by('-created_at')
+    projects = Project.objects.all().order_by('-created_at')[:3]
     return render(request, 'core/home.html', {'projects': projects})
 
 
@@ -11,3 +11,8 @@ def project_detail(request, id):
     project = get_object_or_404(Project, id=id)
     return render(request, 'core/project_detail.html', {'project': project})
 
+def projects_list(request):
+    projects = Project.objects.all().order_by('-created_at')
+    return render(request, 'core/projects.html', {
+        'projects': projects
+    })
